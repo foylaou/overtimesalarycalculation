075b5841724f0180d324104610bce8c4365f4777
Foy <s225002731@gmail.com>
2025-08-29 21:03:03 +0800

    VitePWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vite.dev/config/
+
 export default defineConfig({
   base: '/overtimesalarycalculation/',
   plugins: [react(),
      tailwindcss(),
+      VitePWA({
+          registerType: 'autoUpdate',
+          devOptions: {
+              enabled: true
+          }
+      })
   ],
 })
